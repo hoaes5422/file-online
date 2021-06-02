e22ae3bff452adb7963b9f8b4c28457270c32bc6
Hoaes5422 <dang3638@gmail.com>
2021-06-02 15:41:10 +0700

    Create okna.py

---
+++ okna.py
@@ -0,0 +1,33 @@
+class students:
+
+	def __init__(self, name, major, gpa):
+		self.name = name
+		self.major = major
+		self.gpa = gpa 
+
+class question:
+	def __init__(self, question,answer):
+		self.question = question
+		self.answer = answer
+
+
+# hayda = open("Data.txt", "w")
+
+# tips = {"anh" : "Anh xin lỗi"}
+# x = 0 
+# while x <= 100:
+# 	print(tips["anh"])
+# 	hayda.write("anh xin loi ")
+# 	x +=1 
+
+
+
+
+hayda.close()
+
+
+
+
+
+
+
